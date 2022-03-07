94f74cd819b403358bf8c678332d0caea8214eef
PVH2501 <Pvh250101@gmail.com>
2022-03-07 17:41:53 +0700

    PVH

---
+++ Baithuchanh0703/src/App90.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import static java.lang.System.*;
+public class App90 {
+    public static void main(String[] args) {
+        var sc=new Scanner(in);
+        String chuoi;
+        char kyTu;
+        var scanner = new Scanner(in);
+        out.println("Nhập vào chuỗi bất kỳ: ");
+        chuoi = scanner.nextLine();
+        System.out.println("Các ký tự có trong chuỗi là: ");
+        for (int i = 0; i < chuoi.length(); i++) {
+        	kyTu = chuoi.charAt(i);
+            System.out.println(kyTu);
+        }
+    }
+}
