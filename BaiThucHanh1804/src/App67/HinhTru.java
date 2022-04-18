173af41683a96a133cdf529abfa881d93a198703
PVH2501 <Pvh250101@gmail.com>
2022-04-18 22:33:16 +0700

    PVH

---
+++ BaiThucHanh1804/src/App67/HinhTru.java
@@ -0,0 +1,10 @@
+
+package App67;
+
+public class HinhTru extends HinhTron {
+   
+    @Override
+    public void xuatThongTin() {
+        System.out.println("Đây là Hình Trụ");
+    }
+    }
---
+++ BaiThucHanh1804/src/App75/HinhChuNhat.java
@@ -0,0 +1,7 @@
+package App75;
+
+public class HinhChuNhat extends HinhTron {
+    public HinhChuNhat() {
+        super();
+    }
+    }
