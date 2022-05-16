8cc2a066c83226715f5a53101e3621de9928e2ff
PVH2501 <Pvh250101@gmail.com>
2022-05-16 20:12:41 +0700

    PVH

---
+++ BaiThucHanh09052022/src/App18.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+import java.util.ArrayList;
+public class App18 {
+public static void main( String[] args) {
+	ArrayList<Float> arrListFloat = new ArrayList<> ();
+	arrListFloat.add(0.7f);
+	arrListFloat.add(7.26f);
+	arrListFloat.add(1.02f);
+	arrListFloat.add(9.3f);
+	Iterator<Float> iterator = arrListFloat.iterator();
+	System.out.println(" Các phần tử có trong arrListFloat là : ");
+	while (iterator.hasNext()) {
+		System.out.print(iterator.next() + "\t");
+}
+}
+}
