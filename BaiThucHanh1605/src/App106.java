135ba362f67da95ced5e98eeb416b5157fd98d1b
PVH2501 <Pvh250101@gmail.com>
2022-05-30 11:50:59 +0700

    PVH

--- BaiThucHanh1605/src/App106.java
+++ BaiThucHanh1605/src/App106.java
@@ -1,20 +1,19 @@
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.Scanner;
 
 public class App106 {
+
     public static void main(String[] args) {
-        TreeMap<Integer, Character> treeMap = new TreeMap<>();
-
-        treeMap.put(1, 'a');
-        treeMap.put(2, 'b');
-        treeMap.put(3, 'c');
-        treeMap.put(4, 'd');
-        treeMap.put(5, 'e');
-        treeMap.put(6, 'f');
-
-        Set<Entry<Integer, Character>> SetTreeMap = treeMap.entrySet();
-        System.out.println(" CÁc entry có trong settreemap:");
-        System.out.println(SetTreeMap);
-
+        int m, n;
+        Scanner scanner = new Scanner(System.in);
+        m = scanner.nextInt();
+        n = scanner.nextInt();
+        for (int i = 0; i < m; i++);
+        {
+            for (int j = 0; j < n; j++);
+            {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
     }
 }
